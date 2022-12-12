cae51ee7a8e11d6ec4a2c22b3c836224dcc95746
Asparawr <anbednarczykan@gmail.com>
2022-12-12 21:40:28 +0100

    GEE-55 | button function change

--- lib/widgets/universal_button.dart
+++ lib/widgets/universal_button.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:geeruh/theme.dart';
 
-
-Widget universalButton(double width,double heigth, void Function()? onPressed, [String caption = 'Button']) {
+Widget universalButton(double width, double heigth, Function onPressed,
+    [String caption = 'Button']) {
   return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        minimumSize: Size(width, heigth),
+    style: ElevatedButton.styleFrom(
+      minimumSize: Size(width, heigth),
       backgroundColor: GeeColors.primary1,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(5.0),
       ),
     ),
-      key:  Key(caption),
-      onPressed: onPressed,
-      child:  Text(caption, style: GeeTextStyles.heading6),
-    );
+    key: Key(caption),
+    onPressed: () {
+      onPressed();
+    },
+    child: Text(caption, style: GeeTextStyles.heading6),
+  );
 }
