96dc7124e471877804b787306eb04ee45d1e0d91
ptgreda <piotr.gr3da@gmail.com>
2023-01-05 16:44:11 +0100

    Fixed racing conditions

--- lib/widgets/gee_kanban.dart
+++ lib/widgets/gee_kanban.dart
@@ -1,8 +1,11 @@
 import 'package:appflowy_board/appflowy_board.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:geeruh/main.dart';
 import 'package:geeruh/screens/board/board_store.dart';
 import 'package:geeruh/theme.dart';
 import 'package:geeruh/widgets/gee_build_card.dart';
+import 'package:geeruh/widgets/gee_future_child.dart';
 
 class GeeKanban extends StatefulWidget {
   final List<AppFlowyGroupData> groups;
@@ -29,7 +32,8 @@ class _GeeKanbanState extends State<GeeKanban> {
       // DO NOT REMOVE
       onMoveGroup: (fromGroupId, fromIndex, toGroupId, toIndex) {},
       onMoveGroupItem: (groupId, fromIndex, toIndex) {},
-      onMoveGroupItemToGroup: (fromGroupId, fromIndex, toGroupId, toIndex) {
+      onMoveGroupItemToGroup:
+          (fromGroupId, fromIndex, toGroupId, toIndex) async {
         // TODO when moved between groups it resets order (because of getIssues)
         var fromGroup =
             widget.groups.firstWhere((group) => group.id == fromGroupId);
@@ -40,75 +44,85 @@ class _GeeKanbanState extends State<GeeKanban> {
         String issueId = toGroup.items.isNotEmpty
             ? toGroup.items.elementAt(toIndex).id
             : fromGroup.items.elementAt(fromIndex).id;
-        widget.boardStore.updateIssueStatus(context, issueId, toGroupId);
-        widget.boardStore.getIssues(context);
+        await widget.boardStore.updateIssueStatus(
+            navigatorKey.currentContext!, issueId, toGroupId);
+        await widget.boardStore.getIssues(navigatorKey.currentContext!);
       },
     );
     controller.addGroups(widget.groups);
     super.initState();
   }
 
+  // TODO race because widget is being build before updateIssueStatus is finished
+
   @override
   Widget build(BuildContext context) {
-    controller.clear();
-    controller.addGroups(widget.groups);
-    final config = AppFlowyBoardConfig(
-      groupPadding: const EdgeInsets.symmetric(horizontal: 0.5),
-      groupBackgroundColor: GeeColors.gray9,
-      cornerRadius: 0.0,
-    );
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16.0),
-      child: AppFlowyBoard(
-        controller: controller,
-        cardBuilder: (context, group, groupItem) => AppFlowyGroupCard(
-          key: ValueKey(groupItem.id),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(color: GeeColors.gray1),
-              color: GeeColors.white),
-          child: buildCard(groupItem, widget.boardStore),
-        ),
-        boardScrollController: boardController,
-        // // DO NOT REMOVE
-        // footerBuilder: (context, columnData) => AppFlowyGroupFooter(
-        //       icon: const Icon(Icons.add, size: 20),
-        //       title: const Text('New'),
-        //       height: 50,
-        //       margin: config.groupItemPadding,
-        //       onAddButtonClick: () {
-        //         boardController.scrollToBottom(columnData.id);
-        //       },
-        //     ),
-        headerBuilder: (context, columnData) => Container(
-          decoration: BoxDecoration(
-              color: GeeColors.gray10,
-              border: Border(
-                  bottom: BorderSide(
-                      color: _colorFromId(
-                        columnData.id,
+    return Observer(
+      builder: (_) => GeeFutureChild(
+        status: widget.boardStore.futureGetIssues.status,
+        loaded: () {
+          controller.clear();
+          controller.addGroups(widget.groups);
+          final config = AppFlowyBoardConfig(
+            groupPadding: const EdgeInsets.symmetric(horizontal: 0.5),
+            groupBackgroundColor: GeeColors.gray9,
+            cornerRadius: 0.0,
+          );
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(16.0),
+            child: AppFlowyBoard(
+              controller: controller,
+              cardBuilder: (context, group, groupItem) => AppFlowyGroupCard(
+                key: ValueKey(groupItem.id),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: GeeColors.gray1),
+                    color: GeeColors.white),
+                child: buildCard(groupItem, widget.boardStore),
+              ),
+              boardScrollController: boardController,
+              // // DO NOT REMOVE
+              // footerBuilder: (context, columnData) => AppFlowyGroupFooter(
+              //       icon: const Icon(Icons.add, size: 20),
+              //       title: const Text('New'),
+              //       height: 50,
+              //       margin: config.groupItemPadding,
+              //       onAddButtonClick: () {
+              //         boardController.scrollToBottom(columnData.id);
+              //       },
+              //     ),
+              headerBuilder: (context, columnData) => Container(
+                decoration: BoxDecoration(
+                    color: GeeColors.gray10,
+                    border: Border(
+                        bottom: BorderSide(
+                            color: _colorFromId(
+                              columnData.id,
+                            ),
+                            width: 3))),
+                child: AppFlowyGroupHeader(
+                  title: SizedBox(
+                    child: Text(
+                      columnData.headerData.groupName,
+                      overflow: TextOverflow.clip,
+                      style: GeeTextStyles.heading5.copyWith(
+                        color: _colorFromId(
+                          columnData.id,
+                        ),
                       ),
-                      width: 3))),
-          child: AppFlowyGroupHeader(
-            title: SizedBox(
-              child: Text(
-                columnData.headerData.groupName,
-                overflow: TextOverflow.clip,
-                style: GeeTextStyles.heading5.copyWith(
-                  color: _colorFromId(
-                    columnData.id,
+                    ),
                   ),
+                  height: 50,
+                  margin: config.groupItemPadding,
                 ),
               ),
+              groupConstraints: BoxConstraints.tightFor(
+                  width: MediaQuery.of(context).size.width /
+                      (widget.groups.length + 0.05 * widget.groups.length)),
+              config: config,
             ),
-            height: 50,
-            margin: config.groupItemPadding,
-          ),
-        ),
-        groupConstraints: BoxConstraints.tightFor(
-            width: MediaQuery.of(context).size.width /
-                (widget.groups.length + 0.05 * widget.groups.length)),
-        config: config,
+          );
+        },
       ),
     );
   }
